60a511980043b03475066e3fd05b6345e4759766
Johan <jegomeza2015t@gmail.com>
2021-07-22 21:08:05 -0500

    Carga de proyecto

---
+++ resources/js/contact.js
@@ -0,0 +1,15 @@
+const f_name = document.querySelector("#f-name");
+f_name.required = "required";
+f_name.placeholder = "Nombres Completos";
+
+const l_name = document.querySelector("#l-name");
+l_name.required = "required";
+l_name.placeholder = "Apellidos Completos";
+
+const tel = document.querySelector("#telephone");
+tel.required = "required";
+tel.placeholder = "Númer de Telefono o Celular";
+
+const email = document.querySelector("#email");
+email.required = "required";
+email.placeholder = "example@example.com";
