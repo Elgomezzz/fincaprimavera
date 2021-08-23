a8e3cd04eb47f50931055c11fddf73369a815778
Johan <jegomeza2015t@gmail.com>
2021-08-23 12:46:40 -0500

    INDEX_90%_END

---
+++ resources/js/index.js
@@ -0,0 +1,11 @@
+const img0 = document.getElementById('sc_img_0');
+const img1 = document.getElementById('sc_img_1');
+const img2 = document.getElementById('sc_img_2');
+const img3 = document.getElementById('sc_img_3');
+
+const btn0 = document.getElementById('btn_0');
+const btn1 = document.getElementById('btn_1');
+const btn2 = document.getElementById('btn_2');
+const btn3 = document.getElementById('btn_3');
+
+
